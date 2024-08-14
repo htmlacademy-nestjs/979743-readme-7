2261dc1431935914d0def2a2de301933819e6594
AnnaGW <a.bogoslovskaya@yandex.ru>
2024-08-14 20:23:46 +0500

    Пришлось повозиться с docker. Контейнер в итоге создала. Добавила функцию, которая возвращает строку подключения в mongo. Переписала работу с репозиторием. Все в точности, как в демо. После создания нового пользователя, id в возвращаемом объекте пока пустая строка.

--- project/libs/user/config/src/index.ts
+++ project/libs/user/config/src/index.ts
@@ -2,3 +2,5 @@ export { UserConfigModule } from './user-config.module';
 
 export { default as applicationConfig } from './app.config';
 export { default as dbConfig } from './mongo.config';
+
+export { getMongooseOptions } from './mongo/get-mongoose-options';
